bd7dbdf7018dde758193149d6a2544b88759e2f8
WangWDog <1303319627@qq.com>
2025-06-04 21:36:56 +0800

    初始化项目

--- main_cli.cpp
+++ main_cli.cpp
@@ -1,15 +1,6 @@
-//
-// Created by 13033 on 2025/6/2.
-//
-
 #include <iostream>
-#include <string>
-// #include "main_cli.h"
-int main() {
-    // std::string city;
-    // std::cout << "请输入城市名：";
-    // std::cin >> city;
-    using namespace std;
-    cout << "Hello World!" << endl;
-    return 0;
+
+int main()
+{
+    std::cout << "test";
 }
